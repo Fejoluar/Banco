bf8af32de93c16c05cdeca97b3e6ee619f56d64f
Fejoluar <91299603+Fejoluar@users.noreply.github.com>
2021-09-25 21:21:44 -0300

    Add files via upload

---
+++ ProjetoBancoCariri/src/br/com/felipes/banco/ContaPoupanca.java
@@ -0,0 +1,9 @@
+package br.com.felipes.banco;
+
+public class ContaPoupanca extends Conta {
+
+	public ContaPoupanca(Cliente titular, String numero, String agencia) {
+		super(titular, numero, agencia);
+	}
+
+}
