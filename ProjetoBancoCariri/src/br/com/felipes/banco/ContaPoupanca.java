13166a753592068a47750698ad9bc9ac0974a01f
Felipe Arauo <91299603+Fejoluar@users.noreply.github.com>
2021-09-27 15:26:32 -0300

    Delete ProjetoBancoCariri/src/br/com/felipes/banco directory

--- ProjetoBancoCariri/src/br/com/felipes/banco/ContaPoupanca.java
+++
@@ -1,9 +0,0 @@
-package br.com.felipes.banco;
-
-public class ContaPoupanca extends Conta {
-
-	public ContaPoupanca(Cliente titular, String numero, String agencia) {
-		super(titular, numero, agencia);
-	}
-
-}
